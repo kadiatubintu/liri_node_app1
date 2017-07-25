dddce9290a7824939c0854281bdf57ce80f5d738
Kadiatu <kadibaf@yahoo.com>
2017-07-25 00:21:10 -0400

    Added files:
    - random.txt
    - keys.js
    - liri.js - updated code

--- keys.js
+++ keys.js
@@ -1,8 +1,10 @@
 console.log('this is loaded');
 
-exports.twitterKeys = {
-  consumer_key: '<bQYNkVCo6hNBtzqaq4xVqG6yA>',
-    consumer_secret: '<hwrr5cDANOjI28ROHThYI6gynTYgR8AJJ84KaOriAsJtOt3XUn>',
-      access_token_key: '<885705940149915648-Cuiszud84vaoOOTXiebmJbOGAgpLaCZ>',
-        access_token_secret: '<59COOULURlJOyhPfEVZsgYA4T4DAZ4imPsIe3Q1aSBJas>',
+ var twitterKeys = {
+  consumer_key: 'aMWfkIej3wrPoWGb3gMfpSZDF',
+    consumer_secret: 'HXThPLtmg9V0D1d4BxdTIb694MseOB6mXkZMENTMsRXvnCnKRL',
+      access_token_key: '885705940149915648-Cuiszud84vaoOOTXiebmJbOGAgpLaCZ',
+        access_token_secret: '59COOULURlJOyhPfEVZsgYA4T4DAZ4imPsIe3Q1aSBJas',
 	}
+
+  module.exports = twitterKeys;
--- liri.js
+++ liri.js
@@ -1,43 +1,61 @@
+var request = require("request");
+var fs = require("fs");
 var keys = require("./keys.js");
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
 var second = process.argv[2];
 
-  if(second === "my-tweets"){
+if(second === "my-tweets"){
+  var newTweet = new Twitter({
+    consumer_key: keys.consumer_key,
+    consumer_secret: keys.consumer_secret,
+    access_token_key: keys.access_token_key,
+    access_token_secret: keys.access_token_secret
+    });
+    newTweet.get('statuses/user_timeline', '', function(error, tweets, response){
+      for (var i = 0; i < tweets.length; i++) {
+        console.log(tweets[i].text)
+        console.log("=======================");
+        console.log("=======================");
       }
+    })
+  }
 
-  if(second === "spotify-this-song"){
-      }
+if(second === "spotify-this-song"){
+  var spotify = new Spotify ({
+  id: "88af65e8f9cd475689d14d8218a98357",
+  secret: "e87f206d9cdc456f810c24b2fba3100e",
+   });
+  spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+    }
+    console.log(data);
+    })
+  }
 
-  if(second === "movie-this"){
-      }
+if(second === "do-what-it-says"){
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    console.log(data);
+    })
+  }
 
-  if(second === "do-what-it-says"){
-      }
-
+if(second === "movie-this"){
   var movieName = process.argv[3];
-  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
-  var request = require("request");
-  request(queryUrl, function(error, response, body){
-    if(!error && response.statusCode === 200){
-       //console.log(JSON.parse(body));
-       var movieData = JSON.parse(body);
-      /*var movieData = {
-         title: "Title",
-         year: "Year",
-         imdb: "imdbRating",
-         rating: "Ratings",
-         country: "Country",
-         language: "Language",
-         plot: "Plot",
-         actors: "Actors"
-       }*/
-     };
-     //console.log(JSON.parse(movieData));
-     console.log(movieData.Title);
-     console.log(movieData.Year);
-     console.log(movieData.imdbRating);
-     console.log(movieData.Ratings[1]);
-     console.log(movieData.Country);
-     console.log(movieData.Language);
-     console.log(movieData.Plot);
-     console.log(movieData.Actors);
-});
+  if(movieName ){
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+      request(queryUrl, function(error, response, body){
+        if(!error && response.statusCode === 200){
+           var movieData = JSON.parse(body);
+          }
+       console.log(movieData.Title);
+       console.log(movieData.Year);
+       console.log(movieData.imdbRating);
+       console.log(movieData.Ratings[1]);
+       console.log(movieData.Country);
+       console.log(movieData.Language);
+       console.log(movieData.Plot);
+       console.log(movieData.Actors);
+     })
+   }
+ }
